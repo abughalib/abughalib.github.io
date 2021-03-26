33445d69453ed1ef2460becf7306e5219b2ab72d
abugh <abugh@gmail.com>
2021-03-26 23:45:06 +0530

    css trimmed

--- style_scripts/navbar.js
+++ style_scripts/navbar.js
@@ -1,8 +1,13 @@
-$(window).scroll(function () {
+$(window).scroll(()=> {
   if ($(document).scrollTop() > 50) {
     $('.navbar').addClass('affix');
     console.log("OK");
   } else {
     $('.navbar').removeClass('affix');
   }
+});
+
+$('.toggle-icon').click(()=>{
+  alert("clicked")
+  // $('#nav-container').toggleClass("pushed");
 });
